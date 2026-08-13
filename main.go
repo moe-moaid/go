12808767aca7865e2f496c3da44d852d240e5eba
moe <mohammadmoaid@gmail.com>
2026-08-14 02:10:23 +0500

    more stuff

--- main.go
+++ main.go
@@ -7,13 +7,40 @@ func printHelloWorld() {
 }
 
 func evenOdd() {
-	var input
-	fmt.Print()
-	fmt.Scan(%input)
+	var input int
+	fmt.Print("enter a number")
+	fmt.Scan(&input)
+	var result int = input % 2
+	if(result == 0){
+		fmt.Print("Even")
+	} else {
+		fmt.Print("Odd!")
+	}
+}
+
+
+func calculator() {
+	var first_input, second_input int
+	var operation string
+	fmt.Print("enter first number")
+	fmt.Scan(&first_input)
+	fmt.Print("enter first number")
+	fmt.Scan(&second_input)
+	fmt.Print("enter choose the operation you want to perform")
+	fmt.Scan(&operation)
+	if(operation == "+"){
+		fmt.Printf(first_input + second_input)
+	} else if (operation == "-") {
+		fmt.Printf(first_input - second_input)
+	} else if (operation == "/") {
+		fmt.Printf(first_input / second_input)
+	} else if (operation == "x") {
+		fmt.Printf(first_input * second_input)
+	}
 }
 	
-
-
 func main() {
-	fmt.Print("hello, World!\n")
+	printHelloWorld()
+	evenOdd()
+	calculator()
 }
