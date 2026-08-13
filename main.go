e29ec4edb1b9612143b1527888fff537a2a10612
moe <mohammadmoaid@gmail.com>
2026-08-13 23:02:21 +0500

    init push

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func printHelloWorld() {
+	fmt.Print("Hello, World!\n")
+}
+
+func evenOdd() {
+	var input
+	fmt.Print()
+	fmt.Scan(%input)
+}
+	
+
+
+func main() {
+	fmt.Print("hello, World!\n")
+}
