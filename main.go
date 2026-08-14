57a7f93d18cda1441a98d4502b565d129ad8713e
moe <mohammadmoaid@gmail.com>
2026-08-14 21:46:27 +0500

    some more basics to learn

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"strconv"
+)
 
 func printHelloWorld() {
 	fmt.Print("Hello, World!\n")
@@ -38,9 +42,33 @@ func calculator() {
 		fmt.Println(first_input * second_input)
 	}
 }
-	
+
+func negativeVsPositive() {
+	var input int
+	fmt.Print("Enter a number to test for its sign")
+	fmt.Scan(&input)
+	if(input < 0){
+		fmt.Printf("%d is a negative number", input)
+	} else if (input > 0) {
+		fmt.Printf("%d is a positive number", input)
+	} else {
+		fmt.Print("the number is zero")
+	}
+}
+
+func ageCalc() {
+	var input int
+	current_year, _ := strconv.Atoi(time.Now().Format("2006"))
+	fmt.Println("Enter your birth year")
+	fmt.Scan(&input)
+	fmt.Printf("your age is %d", current_year - input)
+}
+
 func main() {
-	printHelloWorld()
+	/*	printHelloWorld()
 	evenOdd()
 	calculator()
+	negativeVsPositive()
+	*/
+	ageCalc()
 }
